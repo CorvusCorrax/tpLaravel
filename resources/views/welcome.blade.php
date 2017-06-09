814025fbfd7a32f066b5341876687edc61970099
Ubuntu <renaud.cayol@gmail.com>
2017-06-09 08:26:43 +0000

    wle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                BOP    Laravel
+                MOUETTE    Laravel
 		    PHP DE MERDE
                 </div>
 
